9825adff1bb2cea8098857c3108a14561d40f38e
Guilherme Henrique <guilhermehsp26@gmail.com>
2026-09-15 21:02:42 -0300

    Atualizar o App.styles.jsx

--- src/App.styles.jsx
+++ src/App.styles.jsx
@@ -12,7 +12,7 @@ const slideInFromLeft = keyframes`
 `;
 
 export const AppContainer = styled.div`
-    min-height: 100vh;
+    min-height: 100dvh;
     display: flex;
     flex-direction: column;
     align-items: center;
